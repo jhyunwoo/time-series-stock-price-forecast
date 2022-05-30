abc96cca67399353dc4ed2dc791d9f1c40a39cb9
전현우 <jhyunwoo@Ampersand.local>
2022-05-30 22:27:49 +0900

    second commit

--- tensor.py
+++ tensor.py
@@ -1,22 +1,29 @@
 import tensorflow as tf
-
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('gpascore.csv')
-print(data)
 
-
-print(data.isnull().sum())
 data = data.dropna()
-print(data.isnull().sum())
-exit()
+
+y_data = data['admit'].values
+
+x_data = []
+
+for i, rows in data.iterrows():
+    x_data.append([rows['gre'], rows['gpa'], rows['rank']])
+
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(128, activation='softmax'),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
 ])
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit()
+model.fit(np.array(x_data), np.array(y_data), epochs=1000)
+
+predict = model.predict([[750, 3.70, 3], [400, 2.2, 1]])
+
+print(predict)
